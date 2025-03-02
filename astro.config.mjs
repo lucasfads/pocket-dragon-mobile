b582a0c331324c5c19584b290b9d65d342f353d7
Lucas Fads <lucasfads@gmail.com>
2025-03-02 18:51:34 -0300

    fix: Update base path in astro.config.mjs to include leading slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lucasfads.github.io',
-	base: 'pocket-dragon-mobile'
+	base: '/pocket-dragon-mobile'
 });
